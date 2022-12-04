7303ea7b4d72c910d837e9379574f594c1731b5b
AnhTT <gadfly66661@gmail.com>
2022-12-04 13:10:05 +0700

    Login API work

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Request, Post, UseGuards, Inject } from '@nestjs/common';
+import { ApiBody } from '@nestjs/swagger';
 import { LocalAuthGuard } from './auth.guard';
 import { AuthService } from './auth.service';
 
@@ -9,6 +10,20 @@ export class AuthController {
     private service: AuthService,
   ){}
 
+  @ApiBody({
+    schema: {
+      type: "object",
+      properties: {
+        username: {
+          type: "string"
+        },
+        password: {
+          type: "string"
+        }
+      },
+      required: ["username", "password"]
+    }
+  })
   @UseGuards(LocalAuthGuard)
   @Post("/login")
   async login(@Request() req): Promise<any> {
--- src/collection/collection.dto.ts
+++ src/collection/collection.dto.ts
@@ -1,7 +1,14 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 export class CollectionDto {
+  @ApiProperty()
   id: bigint;
+  @ApiProperty()
   title: string;
+  @ApiProperty()
   handle: string;
+  @ApiProperty()
   createdAt: Date;
+  @ApiProperty()
   updatedAt: Date; 
 }
